f058cf98dca28bb19cf1974086d5cf3b97b4d449
BasantElsaey <96740565+BasantElsaey@users.noreply.github.com>
2022-09-16 18:16:41 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ app.set('views',viewsPath)
 
 reques ({
     url, json:true,headers:{"User-Agent" : 'request'}
+    
 },(error,response)=>{
     if(error){
          console.log('Error has happened')
@@ -46,96 +47,3 @@ reques ({
 app.listen(port,() =>{
     console.log(`Example app listening on port ${port}`)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require ('express')
-// const port = process.env.PORT || 3000
-// app.use(express.static(publicDirectory))
-// const path = require('path') // core module
-// const app = express()
-// const hbs = require('hbs')
-
-
-
-// const publicDirectory = path.join(__dirname,'../public')
-// const viewsPath = path.join(__dirname,'../templates/views')
-// const partialsPath = path.join(__dirname,'../templates/partials')
-
-// hbs.registerPartials = partialsPath
-
-// const request = require('request')
-
-
-
-
-
-// app.set('view engine','hbs');
-// console.log(__dirname)
-// app.set('views',viewsPath)
-
-// const url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-08-12&sortBy=publishedAt&apiKey=62de2907a9b5403d8e90d7c485337e73'
-
-// request ({
-//     url,
-// })
-
-
-// // request({url,json:true} ,(error,response) =>{
-// //     // low level error --> internet connection / invalid link
-// //     // const data = JSON.parse(response.body)
-// //     // res.render({data:data})
-
-// //     app.get("/", function (req, res) {
-// //         var user = req.query.getValue;
-
-// //  const url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-08-12&sortBy=publishedAt&apiKey=62de2907a9b5403d8e90d7c485337e73' + user + "&apikey=..."
-
-// //         request(url, function (error, response, body) {
-// //             var data = JSON.parse(body);
-// //             res.render("index", { data: data });
-
-// //             if(error){
-// //                 console.log(' Error has occurred ')
-// //             }
-            
-// //             else if(response.body.error){
-// //                console.log(response.body.error.message)
-// //             }
-// //           else{
-// //             console.log(' Data here ')
-// //           }
-              
-// //         });
-// //     });
-    
-// //         })
-
-// app.get('/',(req,res)=>{
-//     res.render('index',{
-//         title : 'Home Page from hbs',
-//         name : 'Ahmed'
-//     })
-
-// })
-
-
-
-
-// app.listen(port,() =>{
-//     console.log(`Example app listening on port ${port}`)
-// })
-
-
-
